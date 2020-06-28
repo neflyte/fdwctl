c4197466f8907973832185b5cb4e332f4addea9d
Alan Lew <alan@ethereal.cc>
2020-06-27 21:01:09 -0400

    Implement list extensions and servers

--- cmd/fdwctl/cmd/list.go
+++ cmd/fdwctl/cmd/list.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"github.com/elgris/sqrl"
+	"github.com/jackc/pgx/v4"
 	"github.com/neflyte/fdwctl/internal/database"
 	"github.com/neflyte/fdwctl/internal/logger"
 	"github.com/olekukonko/tablewriter"
@@ -13,11 +14,13 @@ import (
 
 var (
 	listCmd = &cobra.Command{
-		Use:     "list <objectType>",
-		Short:   "List objects",
-		PreRunE: preDoList,
-		Run:     doList,
+		Use:      "list <objectType>",
+		Short:    "List objects [server, extension]",
+		PreRunE:  preDoList,
+		PostRunE: postDoList,
+		Run:      doList,
 	}
+	dbConnection *pgx.Conn
 )
 
 type ForeignServersResult struct {
@@ -26,10 +29,30 @@ type ForeignServersResult struct {
 	Owner       string `db:"authorization_identifier"`
 }
 
+type ExtensionsResult struct {
+	Name    string `db:"extname"`
+	Version string `db:"extversion"`
+}
+
 func preDoList(cmd *cobra.Command, args []string) error {
+	var err error
+	log := logger.
+		Root().
+		WithContext(cmd.Context()).
+		WithField("function", "preDoList")
 	if len(args) == 0 {
 		return errors.New("object type is required")
 	}
+	dbConnection, err = database.GetConnection(cmd.Context(), connectionString)
+	if err != nil {
+		log.Errorf("error getting database connection: %s", err)
+		return err
+	}
+	return nil
+}
+
+func postDoList(cmd *cobra.Command, _ []string) error {
+	database.CloseConnection(cmd.Context(), dbConnection)
 	return nil
 }
 
@@ -38,44 +61,76 @@ func doList(cmd *cobra.Command, args []string) {
 	objectType := strings.TrimSpace(args[0])
 	switch objectType {
 	case "server":
-		conn, err := database.GetConnection(cmd.Context(), connectionString)
-		if err != nil {
-			log.Errorf("error getting database connection: %s", err)
-			return
-		}
-		defer database.CloseConnection(cmd.Context(), conn)
-		query, args, err := sqrl.
-			Select("foreign_server_name, foreign_data_wrapper_name, authorization_identifier").
-			From("information_schema.foreign_servers").
-			PlaceholderFormat(sqrl.Dollar).
-			ToSql()
-		rows, err := conn.Query(cmd.Context(), query, args)
-		if err != nil {
-			log.Errorf("error querying for servers: %s", err)
-			return
-		}
-		defer rows.Close()
-		fservers := make([]ForeignServersResult, 0)
-		fserver := new(ForeignServersResult)
-		for rows.Next() {
-			err = rows.Scan(fserver)
-			if err != nil {
-				log.Errorf("error scanning result row: %s", err)
-				return
-			}
-			fservers = append(fservers, *fserver)
-		}
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetHeader([]string{"Name", "Wrapper", "Owner"})
-		for _, svr := range fservers {
-			table.Append([]string{
-				svr.ServerName,
-				svr.WrapperName,
-				svr.Owner,
-			})
-		}
-		table.Render()
+		listServers(cmd)
+	case "extension":
+		listExtension(cmd)
 	default:
 		log.Errorf("unknown object type: %s", objectType)
 	}
+}
+
+func listServers(cmd *cobra.Command) {
+	var err error
+	log := logger.
+		Root().
+		WithContext(cmd.Context()).
+		WithField("function", "listServers")
+	query, _, _ := sqrl.
+		Select("foreign_server_name, foreign_data_wrapper_name, authorization_identifier").
+		From("information_schema.foreign_servers").
+		ToSql()
+	rows, err := dbConnection.Query(cmd.Context(), query)
+	if err != nil {
+		log.Errorf("error querying for servers: %s", err)
+		return
+	}
+	defer rows.Close()
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Wrapper", "Owner"})
+	for rows.Next() {
+		fserver := new(ForeignServersResult)
+		err = rows.Scan(&fserver.ServerName, &fserver.WrapperName, &fserver.Owner)
+		if err != nil {
+			log.Errorf("error scanning result row: %s", err)
+			continue
+		}
+		table.Append([]string{
+			fserver.ServerName,
+			fserver.WrapperName,
+			fserver.Owner,
+		})
+	}
+	table.Render()
+}
+
+func listExtension(cmd *cobra.Command) {
+	log := logger.
+		Root().
+		WithContext(cmd.Context()).
+		WithField("function", "listExtension")
+	query, _, _ := sqrl.
+		Select("extname", "extversion").
+		From("pg_extension").
+		ToSql()
+	rows, err := dbConnection.Query(cmd.Context(), query)
+	if err != nil {
+		log.Errorf("error querying for extensions: %s", err)
+		return
+	}
+	defer rows.Close()
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Name", "Version"})
+	for rows.Next() {
+		ext := new(ExtensionsResult)
+		err = rows.Scan(&ext.Name, &ext.Version)
+		if err != nil {
+			log.Errorf("error scanning result row: %s", err)
+			return
+		}
+		table.Append([]string{
+			ext.Name,
+			ext.Version,
+		})
+	}
+	table.Render()
 }
